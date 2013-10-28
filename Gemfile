776e57c88b5aad96621b6ec69ada8c78756d0b8a
Andrew Baber <baber@thelevelup.com>
2013-10-28 12:07:47 -0400

    revert2

--- Gemfile
+++ Gemfile
@@ -1,5 +1,3 @@
 source 'https://rubygems.org'
 gem 'sinatra', '1.1.0'
 gem 'thin'
-
-ruby ‘2.0.0’
--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Bjornfoods::Application.config.session_store :cookie_store, key: '_bjornfoods_session'
